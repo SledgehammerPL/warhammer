9dcfb1724f1e51c61bf29d115ad4402b29ba01c4
Tomasz Brzezina <tomasz@brzezina.pl>
2026-04-03 14:38:41 +0200

    folders

--- core/settings.py
+++ core/settings.py
@@ -186,8 +186,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/common/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 #TB:
 AUTH_USER_MODEL = 'people.Person'
