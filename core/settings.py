da59edab52f3077562b6c1c7408486c421dbd39e
Tomasz Brzezina <tomasz@brzezina.pl>
2026-07-27 19:47:20 +0200

    Remove WhiteNoise middleware and related settings from core configuration

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -276,5 +275,3 @@ LOGGING = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# To sprawi, że WhiteNoise będzie serwował skompresowane pliki i dbał o cache
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
